d02e2d7a37a305219069ea81ae28ea3af39bbffd
ShahFahadZafar <zafarshahfahad@gmail.com>
2024-05-01 19:57:43 -0700

    Minor changes added

--- index.ts
+++ index.ts
@@ -5,35 +5,62 @@ import chalk from "chalk";
 
 // This is a simple word counter project.
 
-console.log(chalk.bold.magentaBright("\n ----------------------------- Word Counter -------------------------------- "));
-console.log(chalk.bold.magentaBright ("\n Welcome to the word counter project.\n"));
-console.log(chalk.bold.magentaBright("This project will count the number of words and letters in your sentence."));
-console.log(chalk.bold.magentaBright("\n -------------------------------------------------------------- "));
+console.log(
+  chalk.bold.magentaBright(
+    "\n ----------------------------- Word Counter --------------------------------"
+  )
+);
+console.log(
+  chalk.bold.magentaBright("\n \t\t Welcome to the word counter project.\n")
+);
+console.log(
+  chalk.bold.magentaBright(
+    "This project will count the number of words and letters in your sentence."
+  )
+);
+console.log(
+  chalk.bold.magentaBright(
+    "-------------------------------------------------------------------------------"
+  )
+);
 
 while (true) {
-let ans = await inquirer.prompt([
-  {
-    name: "sentence",
-    type: "input",
-    message: "Enter your sentence: ",
-    validate: function (value: string) {
-      if (value.length) {
-        return true;
-      } else {
-        return "Please enter your sentence.";
-      }
+  let ans = await inquirer.prompt([
+    {
+      name: "sentence",
+      type: "input",
+      message: "Enter your sentence: ",
+      validate: function (input: string) {
+        if (input.trim().length > 0) {
+          if (isNaN(parseFloat(input))) {
+            return true;
+          }
+          return "please enter a non-numeric value";
+        }
+        return "please enter a non-empty value";
+      },
     },
-  },
-]);
+  ]);
 
-let words = ans.sentence.trim().split(" ");
-let letters = ans.sentence.trim().split(" ").join("");
-console.log(chalk.bold.greenBright("\n --------------------------------------------------------------------- "));
-console.log(chalk.bold.greenBright(` \n Your sentence has : ${words.length} words and : ${letters.length} letters. \n`));
-console.log(chalk.bold.greenBright("\n ---------------------------------------------------------------------- "));
-};
+  let words = ans.sentence.trim().split(" ");
+  let letters = ans.sentence.trim().split(" ").join("");
+  console.log(
+    chalk.bold.greenBright(
+      "--------------------------------------------------------------------------"
+    )
+  );
+  console.log(
+    chalk.bold.greenBright(
+      `Your sentence has : ${chalk.bold.yellowBright(
+        words.length
+      )} words and : ${chalk.bold.yellowBright(letters.length)} letters. `
+    )
+  );
+  console.log(
+    chalk.bold.greenBright(
+      "--------------------------------------------------------------------------"
+    )
+  );
 
-
-
-
-
+  // console.log(chalk.bold.greenBright(" ---------------------------------------------------------------------- "));
+}
